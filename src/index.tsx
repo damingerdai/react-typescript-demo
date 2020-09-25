864be31601a57539fce97442a7833a5258a3a753
Arthur Ming <guobin.ming@pwc.com>
2020-09-25 17:26:32 +0800

    ci: add eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import * as  React from "react";
+import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./app/App";
-import './style.scss';
+import "./style.scss";
 
 const ROOT = document.querySelector(".container");
 
-const app = <App/>
+const app = <App />;
 
 ReactDOM.render(app, ROOT);
